6d4d0c5bc4ad23dbffee10702c959cfbe8e12f78
Ahsanmehar <ahsanmeharj@gamil.com>
2024-12-18 16:33:07 +0500

    Weather Icon changed

--- src/Weather App/WeatherFinal.jsx
+++ src/Weather App/WeatherFinal.jsx
@@ -1,24 +1,37 @@
 import "./WeatherFinal.css";
 import WeatherData from "./WeatherData";
+import WeatherIcon from "./WeatherIcon";
 
 function WeatherFinal({ weather }) {
+  let icon = "clear.png";
 
-    let icon = "clear.png";
-
-    return (
-        <div id="final">
-            <img src={icon} alt="" />
-        {/* <img src={weather.temp > 15 ? icon : false} id="clear"/> */}
-        <div id="weather-top">
-            <h1 id="temp">{weather.temp}°<span>C</span></h1>
-            <h1 id="city">{weather.city}</h1>
-        </div>
-        <div id="weather-btm">
-        <WeatherData name="humidity" count={weather.humidity} symbol="%" icon="humidity.png"/>
-        <WeatherData name="Wind Speed " count={weather.wind} symbol="km/h" icon="wind.png"/>
-        </div>
-        </ div> 
-    )
+  return (
+    <div id="final">
+      <WeatherIcon temp={weather.temp}/>
+      {/* <img src={icon} alt="" /> */}
+      {/* <img src={weather.temp > 15 ? icon : false} id="clear"/> */}
+      <div id="weather-top">
+        <h1 id="temp">
+          {weather.temp}°<span>C</span>
+        </h1>
+        <h1 id="city">{weather.city}</h1>
+      </div>
+      <div id="weather-btm">
+        <WeatherData
+          name="humidity"
+          count={weather.humidity}
+          symbol="%"
+          icon="humidity.png"
+        />
+        <WeatherData
+          name="Wind Speed "
+          count={weather.wind}
+          symbol="km/h"
+          icon="wind.png"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default WeatherFinal;
---
+++ src/Weather App/WeatherIcon.jsx
@@ -0,0 +1,23 @@
+import "./WeatherIcon.css";
+
+function WeatherIcon({ temp }) {
+  let snow = "snowflake.png";
+  let windy = "windy.png";
+  let clouds = "clouds.png";
+  let partysunny = "partly-sunny.png";
+  let hot = "hot.png";
+  let veryhot = "veryhot.png";
+
+  return (
+    <div id="icons">
+      <img id="clear" src={temp <= 0 ? snow : ""} />
+      <img id="clear" src={temp > 0 && temp <= 10 ? windy : ""} />
+      <img id="clear" src={temp > 10 && temp <= 20 ? clouds : ""} />
+      <img id="clear" src={temp > 20 && temp <= 30 ? partysunny : ""} />
+      <img src={temp > 30 && temp <= 40 ? hot : ""} />
+      <img id="clear" src={temp >= 41 ? veryhot : ""} />
+    </div>
+  );
+}
+
+export default WeatherIcon;
